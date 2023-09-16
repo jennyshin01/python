1cf19a83b797174abfed6df0a09a990a6f6c55d8
jennyshin01 <129889921+jennyshin01@users.noreply.github.com>
2023-09-17 01:44:45 +0900

    d 생성 변수창출
    ㄹㅇ 깃허브 개어렵누

---
+++ VS도형그리기_1(변수).py
@@ -0,0 +1,32 @@
+import turtle as t
+d = 100
+#d에 변수 100을 저장
+
+#삼각형 그리기
+t.color("red")
+t.fd(d)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+#사각형 만들기
+t.color("green")
+t.pensize(3)
+t.fd(100)
+t.lt(90)
+
+t.fd(100)
+t.lt(90)
+
+t.fd(100)
+t.lt(90)
+
+t.fd(100)
+t.lt(90)
+
+#원그리기
+t.color("blue")
+t.pensize(5)
+t.circle(50)
