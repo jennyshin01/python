c632cc5ba2e7a3654d0225004df8fadbb41a57ec
jennyshin01 <129889921+jennyshin01@users.noreply.github.com>
2023-09-17 01:40:13 +0900

    파이썬 공부하기 1일차
    깃허브도 사용한지 1일차
    뭐가 뭔지 솔직히 모른다 ㅇㅈㅋㅋㅋ

---
+++ VS도형그리기.py
@@ -0,0 +1,28 @@
+import turtle as t
+t.color("red")
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+#사각형 만들기
+t.color("green")
+t.pensize(3)
+t.fd(100)
+t.lt(90)
+
+t.fd(100)
+t.lt(90)
+
+t.fd(100)
+t.lt(90)
+
+t.fd(100)
+t.lt(90)
+
+#원그리기
+t.color("blue")
+t.pensize(5)
+t.circle(50)
